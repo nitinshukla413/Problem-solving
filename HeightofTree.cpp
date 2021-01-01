da56c3c4fdc06c7e532b659a20fee1dd3f9e7c2e
Nitin Shukla <50632879+nitinshukla413@users.noreply.github.com>
2021-01-01 19:53:56 +0530

    Create HeightofTree.cpp

---
+++ HeightofTree.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+/*The tree node has data, left child and right child 
+class Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+*/
+    int height(Node* root) {
+        
+        int h1=0,h2=0;
+        if(root==NULL)
+        return 0;        
+        if(root->left)        //compute the Height of each Subtree
+            h1=height(root->left)+1;     
+        if(root->right)
+            h2=height(root->right)+1;                                        
+        return (h1>=h2?h1:h2);        // return the larger Height
+    
+        
+        
+    }
+
+}; //End of Solution
+
+int main() {
+  
+    Solution myTree;
+    Node* root = NULL;
+    
+    int t;
+    int data;
+
+    std::cin >> t;
+
+    while(t-- > 0) {
+        std::cin >> data;
+        root = myTree.insert(root, data);
+    }
+  
+    int height = myTree.height(root);
+    
+  	std::cout << height;
+
+    return 0;
+}
